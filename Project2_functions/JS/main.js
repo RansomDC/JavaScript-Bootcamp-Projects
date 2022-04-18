ccf36f92d1b3ecd3db5cc5b26382a467011ed223
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-04-18 16:45:48 -0700

    Added functions to the Project2 functions files

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+function letsGo() {
+    let str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function shots() {
+    document.getElementById("Shots").innerHTML += " shots";
+}
