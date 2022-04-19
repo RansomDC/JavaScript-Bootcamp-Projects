7222c7f1f51781a28404a709086d3db017370104
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-04-19 11:10:50 -0700

    Added comments

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -3,8 +3,9 @@ const para = document.createElement('p');
 
 let x = 5;
 
+//Data to write to the document giving examples of various math functions and methods.
 document.write(typeof(x));
-document.write(" ");
+document.write(" ");        // Used to add spaces between items for ease of reading.
 document.write(800 + "85");
 document.write(" ");
 document.write(0/0);
@@ -20,6 +21,8 @@ document.write(" ");
 document.write(5 > 4);
 document.write(" ");
 document.write(5 < 4);
+
+//Various console outputs, each with definitions for what each output should be for ease of reading in the console.
 console.log("Math with console.log function");
 console.log(4 + 4);
 console.log("equality (non-type-specific) false");
